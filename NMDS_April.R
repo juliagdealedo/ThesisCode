778e48e99f1677c6ecc3778209f578a132d85bec
juliagdealedo <julia.gdealedo@gmail.com>
2023-04-27 17:39:17 +0200

    Correct NMDS with jaccard and bray, euclidean distances for the barchart and squared colours for the pairs

--- NMDS_April.R
+++ NMDS_April.R
@@ -156,13 +156,14 @@ print(paste("R =", round(etno.ano.use$statistic, 3)))
 hoku <- met.brewer("Hokusai1", n=9)[1:9]
 #grid.col = c("#A6CEE3", "#1F78B4","#B2DF8A","#33A02C",  "#FB9A99",  "#FF7F00", "#FDBF6F", "#E31A1C",  "#CAB2D6",  "#6A3D9A")
 #grid.col = c("#A6CEE3", "#FCD0A1","#5C415D","#1C7293",  "#C16200",  "#FF7F00", "#FDBF6F", "#E31A1C",  "#CAB2D6",  "#6A3D9A")
-grid.col = c( SanCarlos = "#FF7F00", Dicaro="#B2DF8A", Guiyero="#33A02C", Infierno = "#CB6856",Macahua = "#6A3D9A",
-              NuevaVida = "#FDBF6F", Bolivar = "#1F78B4", Tumupasa = "#CAB2D6", Yamino = "#A6CEE3")
-cols <- grid.col[as.factor(resu$Comunidad)]
+grid.col = c( "San Carlos" = "#FF7F00", Dicaro="#B2DF8A", Guiyero="#33A02C", Infierno = "#CB6856",Macahua = "#6A3D9A",
+             "Nueva Vida" = "#FDBF6F", Bolivar = "#1F78B4", Tumupasa = "#CAB2D6", Yamino = "#A6CEE3")
+cols <- grid.col[resu$Comunidad]
 unique(cols)
-[1] "#6A3D9A" "#CAB2D6" "#CB6856" "#1F78B4" "#A6CEE3" "#FF7F00" "#FDBF6F" "#B2DF8A" "#33A02C"
-labels <- c("Macahua", "Tumupasa", "Infierno", "Bolivar", "Yamino", "San Carlos", "Nueva Vida", "Dicaro", "Guiyero")
-unique(resu$Comunidad)
+# pal <- c("#6A3D9A", "#CAB2D6", "#CB6856" ,"#1F78B4", "#A6CEE3" ,"#FF7F00", "#FDBF6F" ,"#B2DF8A", "#33A02C")
+# labels <- c("Macahua", "Tumupasa", "Infierno", "Bolivar", "Yamino", "San Carlos", "Nueva Vida", "Dicaro", "Guiyero")
+# cols <- pal[as.factor(resu$Comunidad)]
+
 # Plot NMDS
 # dev.off()
 # setwd("/Users/juliag.dealedo/ONE/UAM_Doctorado/Capitulos/cap2/figs/figs_2023")
@@ -176,26 +177,44 @@ set.seed(100)
 useMDS <-metaMDS(use_dist, distance="bray", k=2, trymax= 1000) ##k is the number of dimensions
 compMDS <-metaMDS(abs_comp_dist, distance="jaccard", k=2, trymax= 1000, previous.best = useMDS) ##k is the number of dimensions
 
+grid.col = c( "Bolivar" = "#1F78B4", Dicaro="#B2DF8A", Guiyero="#33A02C", Infierno = "#CB6856",Macahua = "#6A3D9A",
+              "Nueva Vida" = "#FDBF6F", "San Carlos" = "#FF7F00", Tumupasa = "#CAB2D6", Yamino = "#A6CEE3")
+cols <- grid.col[resu$Comunidad]
+unique(cols)
+
+
 setwd("/Users/juliag.dealedo/ONE/UAM_Doctorado/Capitulos/cap2/figs/figs_2023")
 svg("nmds_3.svg", height=8,width=13, pointsize=15)
 #pdf("nmds_3.pdf", height=8,width=8, pointsize=12)
 par(mfcol=c(1,2))
 plot(useMDS$points, pch=21, cex=1.5, col="white",bg ="white", cex.axis=1, cex.lab=1 ,xlab="NMDS1", ylab="NMDS2", main="Knowledge")
-legend("topleft", cex=0.8, pch=16, bg="black", col=unique(cols), legend=labels, bty="n", inset = c(0, 0))
+#legend("topleft", cex=0.8, pch=16, bg="black", col=unique(cols), legend=labels, bty="n", inset = c(0, 0))
 points(useMDS$points, pch=21, cex=1.3, col="black", bg = alpha(cols,0.9))
 ordihull(useMDS, resu$Comunidad, col=grid.col, draw="polygon", border="white")
-plot(compMDS$points, pch=21, cex=1.5, col="white",bg ="white", cex.axis=1, cex.lab=1 ,xlab="NMDS1", ylab="NMDS2", main="Floristic")
+plot(compMDS$points, pch=21, cex=1.5, yaxt='n', ann=FALSE, col="white",bg ="white", cex.axis=1, cex.lab=1 ,xlab="NMDS1", ylab="NMDS2", main="Floristic")
 points(compMDS$points, pch=21, cex=1.3, col="black", bg = alpha(cols,0.9))
 ordihull(compMDS, resu$Comunidad, col=grid.col, draw="polygon", border="white")
 dev.off()
 
 
 
+View(resu)
+
+
+# 
+# plot(useMDS$points, pch=21, cex=1.5, col="white",bg ="white", cex.axis=1, cex.lab=1 ,xlab="NMDS1", ylab="NMDS2", main="Knowledge")
+# legend("topleft", cex=0.8, pch=16, bg="black", col=pal, legend=labels, bty="n", inset = c(0, 0))
+# points(useMDS$points, pch=21, cex=1.3, col="black", bg = alpha(cols,0.9))
+# ordihull(useMDS, resu$Comunidad, col=grid.col, draw="polygon", border="white", label = TRUE)
+# #orditorp(useMDS,display="sites",cex=1,air=0.01)
+# points(compMDS$points, pch=21, cex=1.3, col="black", bg =  alpha(cols,0.9))
+# ordihull(compMDS, resu$Comunidad, col=grid.col, draw="polygon", border="black")
+# orditorp(compMDS,display="sites",cex=1.25,air=0.01)
 centro_use <- as.data.frame(t(summary(ordihull(useMDS, resu$Comunidad))))
 centro_comp <-as.data.frame(t(summary(ordihull(compMDS, resu$Comunidad))))
 #comp_euc <- melt(dist(centroids[,1:2], method = "euclidean", diag = F))
 
-plot(centro_comp$NMDS1, centro_comp$NMDS2)
+points(centro_comp$NMDS1, centro_comp$NMDS2, pch=2)
 points(centro_use$NMDS1, centro_use$NMDS2, pch=4)
 
 mat_comp <- as.matrix(dist(centro_comp[,1:2], method = "maximum", diag = F))
@@ -212,10 +231,6 @@ colnames(df_4) <- c("Community1", "Community2", "subtr")
 df_4$group <- paste(df_4$Community1, df_4$Community2, sep="-")
 df_4 <- df_4 %>% mutate (value_fill= ifelse(subtr<0, "Cultural", "Floristic"))
 
-# to_subs <- cbind(comp_euc, use_euc[,3])
-# colnames(to_subs) <- c("Community1", "Community2", "comp", "use")
-# to_subs$subtr <- to_subs$comp - to_subs$use
-# to_subs$group <- paste(to_subs$Community1, to_subs$Community2, sep="-")
 
 bar_predominance <- ggplot(df_4, aes(x=subtr, y=reorder(group,subtr), fill=value_fill))+
   geom_bar(stat='identity')+
@@ -224,16 +239,25 @@ bar_predominance <- ggplot(df_4, aes(x=subtr, y=reorder(group,subtr), fill=value
   theme(axis.title.y=element_blank(),
           axis.text.y=element_blank(),
           axis.ticks.y=element_blank(),
-        panel.background = element_blank())+
+        panel.background = element_blank(),
+        legend.position = c(0.6, 0.2))+
   labs(x = "Difference between cultural and floristic distance", title="Predominant floristic or cultural distance among communities")
 
-#bar_predominance 
+bar_predominance 
 
 pal <- brewer.pal(10, "Paired")
 pal <- c( "#1F78B4", "#B2DF8A", "#33A02C",  "#CB6856","#6A3D9A", "#FDBF6F", "#FF7F00", "#CAB2D6", "#A6CEE3")
 
 grid.col = c( SanCarlos = "#FF7F00", Dicaro="#B2DF8A", Guiyero="#33A02C", Infierno = "#CB6856",Macahua = "#6A3D9A",
               NuevaVida = "#FDBF6F", Bolivar = "#1F78B4", Tumupasa = "#CAB2D6", Yamino = "#A6CEE3")
+
+df_new <- data.frame(Com1=df_4$Community1,
+                      Com2=df_4$Community2,
+                      group=df_4$group,
+                      subtr=df_4$subtr,
+                      Pair1=1,
+                      Pair2=2)
+
 
 
 legend_tile <- ggplot(data = df_4, aes(y=reorder(group,subtr)))+
@@ -244,14 +268,14 @@ legend_tile <- ggplot(data = df_4, aes(y=reorder(group,subtr)))+
          axis.text.x=element_blank(),
          axis.ticks.x=element_blank(),
          panel.background = element_blank(),
-         legend.position = "none")+
+         legend.position = c(-4, 0.2))+
   labs(y="Community pairs", x = "")
 
 legend_tile
 
 
 legend_tile  + bar_predominance
-ggsave ("predominant.svg")
+ggsave ("predominant.svg",height=10,width=8)
 
 
 
